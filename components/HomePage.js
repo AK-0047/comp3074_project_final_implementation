9c2ebe49c9b821d333216d04aa71990675964c7e
Affan <Affanshaikh76112@gmail.com>
2024-12-08 15:25:32 -0500

    home page component added

---
+++ components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import theme from '../utils/theme';
+import CustomButton from '../utils/CustomButton';
+
+const HomePage = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      
+    </SafeAreaView>
+  );
+};
+
+
+
+export default HomePage;
