7396fb99fa78b4cd8a6d988a8746b4592af0a34a
Affan <Affanshaikh76112@gmail.com>
2024-12-08 15:31:59 -0500

    Signup page component created

---
+++ components/SignupScreen.js
@@ -0,0 +1,27 @@
+import React, { useState, useContext } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Alert,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import theme from '../utils/theme';
+import CustomButton from '../utils/CustomButton';
+import { UserContext } from '../context/UserContext'; // Import UserContext
+
+const SignupScreen = ({ navigation }) => {
+  
+  return (
+    <div>
+      
+    </div>
+  )
+    
+};
+
+
+
+export default SignupScreen;
