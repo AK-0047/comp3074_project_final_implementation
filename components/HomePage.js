52b7dc8913236ba9926489ea1391c93eedb7ffd3
Affan <Affanshaikh76112@gmail.com>
2024-12-08 15:27:24 -0500

    Welcome message and navigation to post request added

--- components/HomePage.js
+++ components/HomePage.js
@@ -6,11 +6,18 @@ import CustomButton from '../utils/CustomButton';
 const HomePage = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
-      
+      <View style={styles.main}>
+        <Text style={styles.title}>Welcome to the Home Page</Text>
+
+        <CustomButton
+          title="Post a Request"
+          onPress={() => navigation.navigate('PostRequest')}
+          type="secondary"
+        />
+      </View>
     </SafeAreaView>
   );
 };
-
 
 
 export default HomePage;
