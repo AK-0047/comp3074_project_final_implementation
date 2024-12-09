0ebca1f6a2535bd6434b0c94fe1caddd840da0c9
aryan0322 <156135876+aryan0322@users.noreply.github.com>
2024-12-08 20:59:47 -0500

    Minor changes to app.js

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import SignupScreen from './components/SignupScreen';
 import HomePage from './components/HomePage';
 import PostRequestScreen from './components/PostRequest';
 import TripsPageScreen from './components/TripsPage';
 import AccountPage from './components/AccountPage';
-
 import { TripsProvider } from './context/TripsContext';
 import { UserProvider } from './context/UserContext';
 import Ionicons from 'react-native-vector-icons/Ionicons';
